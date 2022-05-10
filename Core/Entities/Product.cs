e031df4a1bf77a4b8e254d3d0daea751227b5ce2
Jobin George <jobinkbg@gmail.com>
2022-05-10 12:05:30 +0530

    End of section 3

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -3,17 +3,18 @@ namespace Core.Entities;
 /// <summary>
 /// Product Entity
 /// </summary>
-public class Product
+public class Product : BaseEntity
 {
-    /// <summary>
-    /// Primary key Id
-    /// </summary>
-    /// <value></value>
-    public int Id { get; set; }
-
     /// <summary>
     /// Name of the product
     /// </summary>
     /// <value></value>
     public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public string PictureUrl { get; set; }
+    public ProductType ProductType { get; set; }
+    public int ProductTypeId { get; set; }
+    public ProductBrand ProductBrand { get; set; }
+    public int ProductBrandId { get; set; }
 }
--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,4 +16,12 @@ public class StoreContext : DbContext
     }
 
     public DbSet<Product> Products { get; set; }
+    public DbSet<ProductBrand> ProductBrands { get; set; }
+    public DbSet<ProductType> ProductTypes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
 } 
