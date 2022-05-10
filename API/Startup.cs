0a7a8c3c24112a585591b7c9b85f29b7cdf9a5d3
Jobin George <jobinkbg@gmail.com>
2022-05-10 18:02:20 +0530

    End of section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,4 @@
+using API.Helpers;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
@@ -17,6 +18,8 @@ public class Startup
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+        services.AddAutoMapper(typeof(MappingProfiles));
         services.AddControllers();
         services.AddDbContext<StoreContext>(x => 
             x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
@@ -39,6 +42,7 @@ public class Startup
         app.UseHttpsRedirection();
 
         app.UseRouting();
+        app.UseStaticFiles();
 
         app.UseAuthorization();
 
