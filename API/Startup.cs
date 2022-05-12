426309a63fc5be62f6c6d5b40549ccd36397dfd2
Jobin George <jobinkbg@gmail.com>
2022-05-12 19:23:52 +0530

    End of section 6

--- API/Startup.cs
+++ API/Startup.cs
@@ -23,6 +23,13 @@ public class Startup
             x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
         services.AddApplicatonServices();
         services.AddSwaggerDocumentation();
+        services.AddCors(opt =>
+        {
+            opt.AddPolicy("CorsPolicy", policy =>
+            {
+                policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+            });
+        });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,6 +40,7 @@ public class Startup
         app.UseHttpsRedirection();
         app.UseRouting();
         app.UseStaticFiles();
+        app.UseCors("CorsPolicy");
         app.UseAuthorization();
         app.UseSwaggerDocumentation();
 
